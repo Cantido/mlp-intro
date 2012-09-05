ddbe4617f1c77dbab61b3769679b9304bc58f98d
Robert Richter <robert.c.richter@gmail.com>
2012-09-05 08:58:29 -0500

    Set up sigmoid function and variables

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+#define E_CONSTANT = 2.71828
+
+float sigmoid(float x){
+	return (1 / (1 + pow(x, E_CONSTANT));
+}
+
+int main(){
+	float wl1[2] = {0};
+	float wl2[2] = {0};
+	
+	float xl1 = 0;
+	float xl2[2] = {0};
+	float xl3 = 0;
+	
+	return 0;
+}
