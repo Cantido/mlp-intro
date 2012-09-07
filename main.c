26ff7ad0b45cb958bc94428efb3b38221e0910c6
Robert Richter <robert.c.richter@gmail.com>
2012-09-07 10:38:26 -0500

    Implemented the back-propagation algorithm.
    Program is now ready for testing & debugging.

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-#define LEARNING_RATE .5
+#define LEARNING_RATE (0.5)
 
 /* START neural network computational functions */
 
-double sigmoid(double x){
+double f(double x){
 	return (1 / (1 + pow(x, M_E)));
 }
 
-double sigmoid_deriv(double x){
-	return ( x * (1 - x));
+double f_p(double x){
+	return f(x)(1-f(x));
 }
 
 
@@ -18,22 +18,20 @@ double sigmoid_deriv(double x){
 
 int main(){
 
-	/* weights:
-	 * 	[0] = input node's weight on the initial input
-	 * 	[1,2] = the two hidden nodes' weights on the input node's output
-	 * 	[3,4] = the one output node's weights on the two hidden nodes' output
-	 */
-
-	double weight[5] = 0;
-
-	/* net array:
+	/* net:
 	 * 	[0] weighted sum input to input node
 	 * 	[1,2] weighted sum input to hidden nodes
-	 * 	[3] weighted sum input to output node
+	 * 	[3] weighted sum input to output node (NOT FINAL OUTPUT OF NETWORK)
 	 */
 
-	double net[4] = 0;
-	
+	double n[4] = 0;
+
+	/* weights w:
+	 * 	[0,1] = connection between input & first hidden node
+	 * 	[2,3] = connection between input & second hidden node
+	 */
+
+	double w[4] = 0;
 	
 	/* output array:
 	 * 	[0] sigmoid output of input node
@@ -41,66 +39,89 @@ int main(){
 	 * 	[3] sigmoid output of output node (final output of network)
 	 */
 
-	double net[4] = 0;
+	double y[4] = 0;
 	
 	/* error array:
-	 *  [0] local error of input layer
+	 *  [0] dummy variable for index. or the result of messy code. DO NOT USE.
 	 *  [1,2] local error of hidden layer nodes
 	 *  [3] local error of output layer
 	 *
 	 * This array -will- be filled backwards, from 3 to 0
 	 */
 	 
-	 double error[4] = 0;
+	double l_e[4] = 0;
+
+	/* create input/output array */
 
 	double desired[21];
 	double input[21];
 
-	double x = -1.0;
-	for(short i = 0, x = -1.0; i < 21; i++, x + 0.1){
-		input[i] = x;
-		desired[i] = pow(x, 2) + 1;
+	input[0] = -1.0;
+	desired[0] = pow(input[0], 2) + 1;
+
+	for(short i = 1; i < 21; i++){
+		input[i] = input[i-1] + 0.1;
+		desired[i] = pow(input[i], 2) + 1;
 	}
 
-	for(int i = 0; i < 500; i++){ // Completion of one loop: full iteration through dataset
-		for(int j = 0; j < 21; j++){
+	/* done creating input/output array */
 
-			/* first layer: input vector x weight -> sigmoid */
+	double iteration_error = 0;
 
-			net[0] = input[j] * weight[0];
-			results[0] = sigmoid(net[0]);
+	for(int i = 0; i < 500; i++){ // Completion of one loop: one full iteration through dataset
+		for(int j = 0; j < 21; j++){ // Completion of one loop: presentation & propagation of one data point
+			
+			/* input layer computation */
 
-			/* for two nodes: input vector x weight -> sigmoid */
+			n[0] = input[j];
+			y[0] = f(n[0]);
 
-			net[1] = results[0] * weight[1];
-			net[2] = results[0] * weight[2];
-			
-			results[1] = sigmoid(net[1]);
-			results[2] = sigmoid(net[2]);
+			/* hidden layer computation */
 
-			/* for final node: weight * sum both outputs -> sigmoid */
+			n[1] = w[0] * y[0];
+			n[2] = w[1] * y[0];
 
-			net[3] = results[2] * weight[3] + 
-			
-			/* back-propagate */
-			/* compute output layer local error */
-			
-			error[3] = (desired[j] - results[3]) * sigmoid_deriv(results[3]);
-			
-			/* compute hidden layer local error */
-			
-			error[1] = sigmoid_deriv(net[1]) * error [3] * weight[3];
-			error[2] = sigmoid_deriv(net[2]) * error [3] * weight[4];
-			
-			/* compute input layer local error */
-			
-			error[0] = sigmoid_deriv(net[0]) * ((error[1] * weight[1]) + (error[2] * weight[2]));
-			
-			/* update weights (i'll go from input layer to output layer) */
-			
-			
+			y[1] = f(n[1]);
+			y[2] = f(n[2]);
 
+			/* output layer computation */
+
+			n[3] = (w[2] * y[1]) + (w[3] * y[2]);
+
+			y[3] = f(n[3]);
+
+			/* the output of the network is now stored in y[3]
+
+			/* back propagating now */
+
+			/* compute error of output layer */
+
+			l_e[3] = (desired[j] - y[3]) * f_p(n[3]);
+
+			/* compute local error of hidden layer */
+
+			l_e[1] = y[1](1 - y[1]) * l_e[3] * w[2];
+			l_e[2] = y[2](1 = y[1]) * l_e[3] * w[3];
+
+			/* there is no input layer local error term */
+
+			/* weight updates */
+
+			/* input-hidden connection weights */
+
+			w[0] = w[0] + LEARNING_RATE * l_e[1] * y[0];
+			w[1] = w[1] + LEARNING_RATE * l_e[2] * y[0];
+
+			/* hidden-output connection weights */
+
+			w[2] = w[2] + LEARNING_RATE * l_e[3] * y[1];
+			w[3] = w[3] + LEARNING_RATE * l_e[3] * y[2];
+			
+			
+			iteration_error += pow((desired[j] - y[3]), 2);
 		}
+
+		printf("%i \t%f", i, iteration_error);
 
 	}
 
